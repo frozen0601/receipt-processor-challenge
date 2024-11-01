d1699b79653fe5ac165345da5e01096537b49fe3
frozen0601 <71y716andy@gmail.com>
2024-10-31 23:06:12 -0400

    add django-redis to support django-ratelimit

--- backend/receipts/urls.py
+++ backend/receipts/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ReceiptViewSet
 
 router = DefaultRouter()
-router.register(r"receipts", ReceiptViewSet, basename="receipt")
+router.register(r"", ReceiptViewSet, basename="receipt")
 
 urlpatterns = [
     path("", include(router.urls)),
